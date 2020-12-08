00e17af6910ef2ab01d534ee55e396ef5f736ae0
Nicole Hamilton <Nicole@NicolesLaptop.local>
2020-12-08 09:48:30 -0800

    [movie.js] add component for movies, then added them to app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie'
+
+const movie= {
+
+}
 
 function App() {
   return (
@@ -17,6 +22,9 @@ function App() {
         >
           Learn React
         </a>
+        <Movie title="The Fellowship of the Ring" hours="2" minutes="58" />
+        <Movie title="The Two Towers" hours="2" minutes="59" />
+        <Movie title="The Return of the King" hours="3" minutes="21" />
       </header>
     </div>
   );
